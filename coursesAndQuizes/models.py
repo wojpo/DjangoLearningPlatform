5955351527509c5853c26e3432e12d3e78a812cb
Wojciech Piech <ilikeahottodogu@gmail.com>
2024-05-29 00:34:27 +0200

    models prepared

--- coursesAndQuizes/models.py
+++ coursesAndQuizes/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Course(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
 
 
 class Lesson(models.Model):
@@ -25,3 +26,21 @@ class UserLesson(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     completed = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+class SubQuiz(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    question = models.CharField(max_length=200)
+
+
+class Answer(models.Model):
+    subQuiz = models.ForeignKey(SubQuiz, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=200)
+    is_correct = models.BooleanField()
